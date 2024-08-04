2e1bcddb98e3579fdc12ccb61428cd741e484fa0
anastasia <anastasiaverbenets@gmail.com>
2024-08-04 17:29:28 +0300

    task-2 update

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -9,20 +9,18 @@ function onFormSubmit(e) {
 
     const formData = {
         name: e.target.name.value,
-        message: e.target.message.value,
+        message: e.target.message.value
     }
 
     localStorage.setItem('feedbackData', JSON.stringify(formData));
-
     feedbackForm.reset();
-}
+};
 
 function getLocalStorage() {
     const savedData = localStorage.getItem('feedbackData');
 
     if (savedData) {
         const parsedData = JSON.parse(savedData);
-
         feedbackForm.elements.name.value = parsedData.name;
         feedbackForm.elements.message.value = parsedData.message;
     }
